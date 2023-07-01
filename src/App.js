7b48d092bb7818ae1eef1952549a11c5f7b963d1
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2023-07-01 18:10:01 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
+import { Box } from '@mui/material';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <Box>
+      <h1><AutoAwesomeIcon />MCR-5</h1>
+
+      </Box>
     </div>
   );
 }
